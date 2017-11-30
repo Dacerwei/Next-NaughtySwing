b3dde9c10ef20f3b9ab01a15c43687baeace6e75
Chinwei.Hsu <chinwei.hsu@kkday.com>
2017-11-30 18:09:11 +0800

    add content to portfolio page

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import Layout from '../containers/Layout';
+import PortfolioSets from '../data/PortfolioSets';
+import _ from 'lodash';
 
 export default class Portfolio extends React.Component {
     render() {
         return(
             <div className="page-container">
                 <Layout>
-                    <h1>Portfolio</h1>
+                {
+                    _.map(PortfolioSets, (portfolio) => {
+                        return(
+                            <figure className="portfolio-wrapper">
+                                <img className="portfolio-img"
+                                    src={portfolio.src}/>
+                                <figcaption className="portfolio-figcaption">
+                                    <h2>{portfolio.title}</h2>
+                                </figcaption>
+                            </figure>
+                        );
+                    })
+                }
                 </Layout>
+                <style jsx>{`
+                    .portfolio-img {
+                        width: 100%;
+                    }
+                `}</style>
             </div>
         );
     }
