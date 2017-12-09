eda5798b55c15ce53a5b3e3f1a2de675f6ca4b8f
Chinwei.Hsu <chinwei.hsu@kkday.com>
2017-12-09 21:40:03 +0800

    portfolio detail 增加

--- pages/portfolioitem.js
+++ pages/portfolioitem.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Layout from '../containers/Layout';
 import PortfolioSets from '../data/PortfolioSets';
+import Youtube from 'react-youtube';
 
 const PortfolioItemPage = (props) => {
     const PortfolioItem = PortfolioSets[props.url.query.id];
@@ -8,12 +9,47 @@ const PortfolioItemPage = (props) => {
         <div className="page-container">
             <Layout>
                 <div className="item-wrapper">
-                    <h1>{PortfolioItem.title}</h1>
-                    <p>{PortfolioItem.chineseDescription}</p>
-                    <p>{PortfolioItem.englishDescription}</p>
-                    <p>{PortfolioItem.youtubeVideoID}</p>
+                    <div className="title-wrapper">
+                        <h1 className="title">{PortfolioItem.title}</h1>
+                    </div>
+                    {
+                        PortfolioItem.youtubeVideoID &&
+                        <div className="video-wrapper">
+                            <Youtube
+                                videoId={PortfolioItem.youtubeVideoID}
+                                id={PortfolioItem.youtubeVideoID}
+                                className="youtubevideo"
+                                opts={{
+                                    width: '100%',
+                                    height: 'auto',
+                                }}
+                            />
+                        </div>
+                    }
+                    <div className="content-wrapper">
+                        <p className="content ch">{PortfolioItem.chineseDescription}</p>
+                        <p className="content en">{PortfolioItem.englishDescription}</p>
+                    </div>
                 </div>
             </Layout>
+            <style jsx>{`
+                .title-wrapper {
+                    text-align: center;
+                    padding: 2%;
+                }
+                .title {
+                    font-size: 20pt;
+                    color: orange;
+                }
+                .content-wrapper {
+                    padding: 2%;
+                }
+                .conent {
+                    font-size: 12pt;
+                    line-height: 2em;
+                }
+
+            `}</style>
         </div>
     );
 }
