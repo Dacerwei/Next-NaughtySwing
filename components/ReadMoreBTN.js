43ea35847209b55cc200901df3c6c1bfcada1e8f
Chinwei.Hsu <chinwei.hsu@kkday.com>
2017-12-18 15:10:48 +0800

    read more button refactoring

--- components/ReadMoreBTN.js
+++ components/ReadMoreBTN.js
@@ -1,33 +1,47 @@
 import React from 'react';
 import Link from 'next/link';
+import PropTypes from 'prop-types';
 
-const ReadMoreBTN = (props) => {
-    return(
-        <div className="read-more-wrapper">
-            <Link href={props.url} target={props.tartget}>
-                <a>{props.label}</a>
-            </Link>
-            <style jsx>{`
-                .read-more-wrapper {
-                    text-align: center;
-                    background-color: rgba(113,200,213,0.8);
-                    width: 20%;
-                    margin: 0 auto;
-                    border-radius: 20px;
-                    margin-top: 20px;
-                    margin-bottom: 20px;
-                }
-                .read-more-wrapper a {
-                    text-decoration-line: none;
-                    color: white;
-                    font-size: 12pt;
-                    line-height: 1.5em;
-                    letter-spacing: 1px;
-                    cursor: pointer;
-                }
-            `}</style>
-        </div>
-    );
+class ReadMoreBTN extends React.Component {
+    render() {
+        return(
+            <div className="read-more-wrapper">
+                <Link href={this.props.url}>
+                    <a target={this.props.target}>{this.props.label}</a>
+                </Link>
+                <style jsx>{`
+                    .read-more-wrapper {
+                        text-align: center;
+                        background-color: rgba(113,200,213,0.8);
+                        width: 20%;
+                        margin: 0 auto;
+                        border-radius: 20px;
+                        margin-top: 20px;
+                        margin-bottom: 20px;
+                    }
+                    .read-more-wrapper a {
+                        text-decoration-line: none;
+                        color: white;
+                        font-size: 12pt;
+                        line-height: 1.5em;
+                        letter-spacing: 1px;
+                        cursor: pointer;
+                    }
+                `}</style>
+            </div>
+        );
+    }
+}
+
+ReadMoreBTN.defaultProps = {
+    url: null,
+    target: "self",
+    label: null,
 };
 
+ReadMoreBTN.propTypes = {
+    url: PropTypes.string,
+    target: PropTypes.string,
+    label: PropTypes.string
+};
 export default ReadMoreBTN;
